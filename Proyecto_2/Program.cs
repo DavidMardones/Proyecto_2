73e972aeea98b0976ce4170364362b844f567227
David Mardones <david.mardones04@inacapmail.cl>
2023-04-18 11:43:19 -0400

    appconfig

--- Proyecto_2/Program.cs
+++ Proyecto_2/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Proyecto_2.Comunicacion;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,20 @@ namespace Proyecto_2
     {
         static void Main(string[] args)
         {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+
+            Console.WriteLine("Iniciando servidor en puerto {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            if (servidor.Iniciar())
+            {
+                //Ok, puede conectar
+                Console.WriteLine("Servidor iniciado");
+            }
+            else
+            {
+                Console.WriteLine("ERROR, EL PUERTO ESTA {0} ESTA EN USO", puerto);
+            }
         }
     }
 }

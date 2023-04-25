59acd886830d47021ef556a8d305f947df2c0ff7
David Mardones <david.mardones04@inacapmail.cl>
2023-04-25 12:17:18 -0400

    cliente servidor

--- ClienteMundito/Program.cs
+++ ClienteMundito/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using ClienteMundito.Comunicacion;
 
 namespace ClienteMundito
 {
@@ -10,6 +12,28 @@ namespace ClienteMundito
     {
         static void Main(string[] args)
         {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            string servidor = ConfigurationManager.AppSettings["servidor"];
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Conectando a Servidor {0} en puerto {1}", servidor, puerto);
+            ClienteSocket clienteSocket = new ClienteSocket(servidor, puerto);
+            if (clienteSocket.Conectar())
+            {
+                Console.WriteLine("Conectado...");
+                //Recibir lo que envia el servidor
+                string mensaje = clienteSocket.Leer();
+                Console.WriteLine("M: {0}", mensaje);
+                string nombre = Console.ReadLine().Trim();
+                clienteSocket.Escribir(nombre);
+                mensaje = clienteSocket.Leer();
+                Console.WriteLine("M: {0}", mensaje);
+                clienteSocket.Desconectar();
+            }
+            else
+            {
+                Console.WriteLine("Error de comunicacion");
+            }
+            Console.ReadKey();
         }
     }
 }
